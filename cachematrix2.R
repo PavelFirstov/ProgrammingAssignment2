80bbd03d2a73f33db199c87cca9687eda09da4d9
Pavel Firstov <pfirstov@gmail.com>
2022-06-01 15:53:48 +0300

    From R studio

---
+++ cachematrix2.R
@@ -0,0 +1,32 @@
+setwd('D:\R_programms\ProgrammingAssignment2')
+##
+ # We make a sample matrix 
+## s = NULL
+## set"mean" = "solve"
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+##
+## 
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("inversed matrix: ")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
+
